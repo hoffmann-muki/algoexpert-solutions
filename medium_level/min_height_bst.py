a0717d88d943008f2ba9cef913e3b13256b18b63
hoffmann-muki <s1894401@ed.ac.uk>
2024-07-05 06:31:26 +0100

    added min height bst and bst traversal solutions

---
+++ medium_level/min_height_bst.py
@@ -0,0 +1,50 @@
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+
+# O(nlogn) time | O(n) space
+def minHeightBst(array):
+    return constructMinHeightBst(array, None, 0, len(array)-1)
+
+def constructMinHeightBst(array, bst, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    valueToAdd = array[midIdx]
+    if bst is None:
+        bst = BST(valueToAdd)
+    else:
+        bst.insert(valueToAdd)
+    constructMinHeightBst(array, bst, startIdx, midIdx-1)
+    constructMinHeightBst(array, bst, midIdx+1, endIdx)
+    return bst
+
+# *********************** Alternate Solution *************************
+
+# O(n) time | O(n) space
+def minHeightBst(array):
+    return constructMinHeightBst(array, 0, len(array)-1)
+
+def constructMinHeightBst(array, startIdx, endIdx):
+    if endIdx < startIdx:
+        return None # leaf node
+    midIdx = (startIdx + endIdx) // 2
+    bst = BST(array[midIdx])
+    bst.left = constructMinHeightBst(array, startIdx, midIdx-1)
+    bst.right = constructMinHeightBst(array, midIdx+1, endIdx)
+    return bst
+
